87d49976b88545c740cd0ecfeff581dfa1af8756
Han Han <haannn@qq.com>
2022-06-14 05:53:08 +0000

    feat: 初始化caret组件

---
+++ src/app/caret/caret.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caret',
+  templateUrl: './caret.component.html',
+  styleUrls: ['./caret.component.css']
+})
+export class CaretComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/input-exercise/input-exercise.module.ts
+++ src/app/input-exercise/input-exercise.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputExerciseComponent } from './input-exercise.component';
 import { InputItemComponent } from '../input-item/input-item.component';
+import { CaretComponent } from '../caret/caret.component';
 
 @NgModule({
-  declarations: [InputExerciseComponent, InputItemComponent],
+  declarations: [InputExerciseComponent, InputItemComponent, CaretComponent],
   exports: [InputExerciseComponent],
   imports: [CommonModule],
 })
